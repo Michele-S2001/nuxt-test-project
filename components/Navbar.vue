344226bfde2bc17f76b603abbef21985c8bb99e7
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-19 14:57:44 +0200

    Add - Basic navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <nav class="py-4">
+    <ul>
+      <li>
+        <NuxtLink to="/style-demo">Colors & Typography</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@
 
   <section class="pt-8 h-full">
     <div class="container text-center">
-      <h1 class="text-5xl">Nuxt test project</h1>
+      <h1 class="text-5xl mb-5">Nuxt test project</h1>
+      <Navbar />
     </div>
   </section>
 
