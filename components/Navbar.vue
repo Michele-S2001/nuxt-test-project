f925649208c4d32410a9d01db3d8394dc27fd6fb
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-19 16:39:28 +0200

    Add - a new link to Navbar component & some hover style

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
 
   <nav class="py-4">
-    <ul>
-      <li>
+    <ul class="flex gap-3 flex-wrap">
+      <li class="hover:text-blue-700 hover:underline">
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li class="hover:text-blue-700 hover:underline">
         <NuxtLink to="/style-demo">Colors & Typography</NuxtLink>
       </li>
     </ul>
