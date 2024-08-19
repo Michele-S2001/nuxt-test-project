c4ca3ef0362c2c2cf3e89ba1823229e2c16e16a1
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-19 16:46:35 +0200

    Add - Typography part in the  style-demo page

---
+++ components/TypographySample.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+const props = defineProps({
+  size: {
+    type: String as PropType<'s' | 'm' | 'b'>,
+    default: 'm'
+  }
+})
+
+const fontSizeDetails = computed(() => {
+  switch(props.size) {
+    case 'b':
+      return {
+        label: 'titan',
+        styleClass: 'text-titan'
+      }
+    case 'm':
+      return {
+        label: 'intermediate',
+        styleClass: 'text-intermediate'
+      }
+    case 's':
+      return {
+        label: 'classic',
+        styleClass: 'text-classic'
+      }
+    default:
+      return {
+        label: 'intermediate',
+        styleClass: 'text-intermediate'
+      }
+  }
+})
+
+</script>
+
+<template>
+
+  <div class="mb-10">
+    <span class="block uppercase text-mistGray">{{ fontSizeDetails.label }}</span>
+    <p :class="fontSizeDetails.styleClass" >Lorem abcdefghij...</p>
+  </div>
+
+</template>
---
+++ pages/style-demo/index.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <header>
+    <div class="container">
+      <Navbar />
+    </div>
+  </header>
+
+  <section class="pt-5">
+    <div class="container">
+      <h1 class="text-4xl mb-10">Colors & Typography</h1>
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <!-- TYPOGRAPHY -->
+        <article>
+          <header class="mb-8">
+            <Subheading>Typography</Subheading>
+          </header>
+          <TypographySample size="b" />
+          <TypographySample size="m" />
+          <TypographySample size="s" />
+        </article>
+        <!-- COLORS -->
+        <article>
+          <header class="mb-5">
+            <Subheading>Colors</Subheading>
+          </header>
+        </article>
+      </div>
+    </div>
+  </section>
+
+</template>
