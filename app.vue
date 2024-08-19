58b8e5afd1cbf54cd7eedef1b83e9b8146ddf952
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-19 14:48:16 +0200

    Feat - Nuxt Routing

--- app.vue
+++ app.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1 class="text-lime-700">Hello nuxt project</h1>
+  <NuxtPage/>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+
+  <section class="pt-8 h-full">
+    <div class="container text-center">
+      <h1 class="text-5xl">Nuxt test project</h1>
+    </div>
+  </section>
+
+</template>
