b4732e48bec52277e3dfd8fad4232db3ee655c57
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-20 18:46:23 +0200

    Refactor - dynamic link generation in the navbar and also style changes

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,36 @@
+<script lang="ts" setup>
+
+interface navLink {
+  uri: string,
+  pageName: string
+}
+
+const navLinks:navLink[] = [
+  {
+    uri: '/',
+    pageName: 'home'
+  },
+  {
+    uri: '/style-demo',
+    pageName: 'colors & typography'
+  },
+  {
+    uri: '/state-demo',
+    pageName: 'state menagement'
+  },
+]
+
+</script>
+
+
 <template>
 
   <nav class="py-4">
     <ul class="flex gap-3 flex-wrap">
-      <li class="hover:text-blue-700 hover:underline capitalize">
-        <NuxtLink to="/">Home</NuxtLink>
-      </li>
-      <li class="hover:text-blue-700 hover:underline capitalize">
-        <NuxtLink to="/style-demo">Colors & Typography</NuxtLink>
-      </li>
-      <li class="hover:text-blue-700 hover:underline capitalize">
-        <NuxtLink to="/state-demo">State menagement</NuxtLink>
+      <li 
+        v-for="(link, i) in navLinks" :key="i" 
+        class="hover:text-blue-700 capitalize bg-crimson text-white font-semibold leading-10 px-3 rounded-3xl text-lg shadow-xl">
+        <NuxtLink :to="link.uri">{{ link.pageName }}</NuxtLink>
       </li>
     </ul>
   </nav>
