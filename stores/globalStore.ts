8ccad5e1266fe9e49ee5935e7158b0effb1cd881
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-20 10:06:01 +0200

    Add - setMessage method to update 'message' value in globalStore

--- stores/globalStore.ts
+++ stores/globalStore.ts
@@ -3,5 +3,13 @@ import { defineStore } from "pinia";
 export const useGlobalStore = defineStore('globalStore', {
   state: () => ({
     message: 'Hello World!'
-  })  
+  }),
+
+  actions: {
+    setMessage(text: string) {
+      if(text.trim().length > 0) {
+        this.message = text;
+      }
+    }
+  }
 })
