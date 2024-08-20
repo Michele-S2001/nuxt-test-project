dc8313e0e891f089c15c545e2aa22d073c17eeea
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-20 10:20:47 +0200

    Add - new link to Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,11 +2,14 @@
 
   <nav class="py-4">
     <ul class="flex gap-3 flex-wrap">
-      <li class="hover:text-blue-700 hover:underline">
+      <li class="hover:text-blue-700 hover:underline capitalize">
         <NuxtLink to="/">Home</NuxtLink>
       </li>
-      <li class="hover:text-blue-700 hover:underline">
+      <li class="hover:text-blue-700 hover:underline capitalize">
         <NuxtLink to="/style-demo">Colors & Typography</NuxtLink>
+      </li>
+      <li class="hover:text-blue-700 hover:underline capitalize">
+        <NuxtLink to="/state-demo">State menagement</NuxtLink>
       </li>
     </ul>
   </nav>
