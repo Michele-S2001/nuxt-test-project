824dc032b956a461bbf813dc83479d3917cf3f9c
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-20 10:23:05 +0200

    Add - initial page title for basic link functionality

---
+++ pages/state-demo/index.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <header>
+    <div class="container">
+      <Navbar />
+    </div>
+  </header>
+
+  <section class="py-5">
+    <div class="container">
+      <h1 class="text-4xl mb-10 capitalize">Global state menagement</h1>
+    </div>
+  </section>
+
+</template>
