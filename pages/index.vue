cdce1724d19f9bb6e185342f26d5a87b2f4cee2a
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-20 18:43:55 +0200

    Add - a little brief of the project in the homepage and some style changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,13 @@
 
 <template>
 
-  <section class="pt-8 h-full">
+  <section class="pt-8">
     <div class="container text-center">
-      <h1 class="text-5xl mb-5">Nuxt test project</h1>
-      <Navbar />
+      <h1 class="text-5xl mb-24">Nuxt test project</h1>
+      <p class="text-xl tracking-wide mb-10">This project demonstrates different features using nuxt, tailwind, and pinia</p>
+      <div class="flex justify-center">
+        <Navbar />
+      </div>
     </div>
   </section>
 
