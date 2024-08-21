9ecf969dc2e4f2a3a2dff3653e57db8c08bf34b6
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-21 10:56:41 +0200

    Style - added some letter-spacing to subheading component

--- components/Subheading.vue
+++ components/Subheading.vue
@@ -1,7 +1,7 @@
 <template>
 
   <h2 
-    class="text-mistGray uppercase font-semibold after:content-[''] after:block after:bg-mistGray after:w-full after:h-0.5">
+    class="text-mistGray uppercase font-semibold after:content-[''] after:block after:bg-mistGray after:w-full after:h-0.5 tracking-wider">
     <slot></slot>
   </h2>
 
