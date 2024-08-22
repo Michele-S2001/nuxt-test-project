b4202a0aa903b6cf1146bd1d78f9d4bc57dd9cdf
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-22 09:55:19 +0200

    Style - small style changes on navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -33,8 +33,8 @@ const navLinks:navLink[] = [
     <ul class="flex gap-3 flex-wrap">
       <li 
         v-for="(link, i) in navLinks" :key="i" 
-        class="hover:text-blue-700 capitalize bg-crimson text-white font-semibold leading-10 px-3 rounded-3xl text-lg shadow-xl">
-        <NuxtLink :to="link.uri">{{ link.pageName }}</NuxtLink>
+        class="transition cursor-pointer ease-in-out duration-200 hover:scale-110 active:scale-95 active:transition active:duration-100 active:ease-in-out capitalize bg-crimson text-white font-semibold rounded-3xl text-lg shadow-xl">
+        <NuxtLink :to="link.uri" class="leading-10 px-3">{{ link.pageName }}</NuxtLink>
       </li>
     </ul>
   </nav>
