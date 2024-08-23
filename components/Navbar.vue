1bcb479d0a6fa4d9945407c013e87e6d17e7a7aa
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-23 09:53:39 +0200

    Sytle - navlink bg change

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -33,7 +33,7 @@ const navLinks:navLink[] = [
     <ul class="flex gap-3 flex-wrap">
       <li 
         v-for="(link, i) in navLinks" :key="i" 
-        class="transition cursor-pointer ease-in-out duration-200 hover:scale-110 active:scale-95 active:transition active:duration-100 active:ease-in-out capitalize bg-crimson text-white font-semibold rounded-3xl text-lg shadow-xl">
+        class="transition cursor-pointer ease-in-out duration-200 hover:scale-110 active:scale-95 active:transition active:duration-100 active:ease-in-out capitalize text-black border border-neutral-100 font-semibold rounded-3xl text-lg shadow-xl">
         <NuxtLink :to="link.uri" class="leading-10 px-3">{{ link.pageName }}</NuxtLink>
       </li>
     </ul>
