557655a7abf318d3bd9d54c5953a8e20ba3620a6
MicheleSerafini <serafini.michele01@gmail.com>
2024-08-23 10:38:05 +0200

    Add - used tecnologies icons logos inside homepage

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,12 @@ useHead({
 
   <section class="pt-8">
     <div class="container text-center">
-      <h1 class="text-5xl mb-24">Nuxt test project</h1>
+      <h1 class="text-5xl mb-9 font-semibold">Nuxt test project</h1>
+      <div class="flex gap-8 sm:gap-10 justify-center py-10">
+        <img class="max-w-14"src="/public/imgs/nuxt-logo.svg" alt="Nuxt logo image">
+        <img class="max-w-14"src="/public/imgs/pinia-logo.svg" alt="Pinia logo image">
+        <img class="max-w-14"src="/public/imgs/tailwindcss-logo.svg" alt="Tailwind css logo image">
+      </div>
       <p class="text-xl tracking-wide mb-10">This project demonstrates different features using nuxt, tailwind, and pinia</p>
       <div class="flex justify-center">
         <Navbar />
